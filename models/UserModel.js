c8a4250007bb43b2925c5503594d9dd115e004eb
panzhilov <panzhilov@gmail.com>
2023-05-17 16:35:28 +0300

    update User model

--- models/UserModel.js
+++ models/UserModel.js
@@ -23,6 +23,6 @@ const UserSchema = new Schema({
   resetToken: { type: String },
 
   expireToken: { type: Date }
-});
+}, {timestamps: true});
 
 module.exports = mongoose.model('User', UserSchema);

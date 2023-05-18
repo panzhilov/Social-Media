9cbfdbbb963610291b29d4e6b3c2d213bb61768c
panzhilov <panzhilov@gmail.com>
2023-05-18 12:41:14 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,21 @@
 import axios from "axios";
+import baseUrl from "../utils/baseUrl";
+import { useEffect } from "react";
 
-function Index({posts}) {
-    return(
-        
+
+function Index({user, userFollowStats}) {
+
+    useEffect(() => {
+        document.title = `Welcome ,${user.name.split(" ")[0]}`;
+    },[])
+
+    return( 
         <div>
-          {posts && posts.length>0 && posts.map(post=><h1 key={post._id}>{post.title}</h1>)}
+         Homepage
         </div>
     )
 }
 
-Index.getInitialProps = async ctx => {
-    try {
-        const res = await axios.get('https://jsonplaceholder.typicode.com/posts')
 
-        return {posts: res.data}
-    } catch (error) {
-        return {errorLoading: true}
-    }
-}
 
 export default Index;

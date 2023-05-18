be0f43ceb592ef754598c5810c8d1f0c4b7e99d0
panzhilov <panzhilov@gmail.com>
2023-05-18 13:37:17 +0300

    Add functionality to Posts - Create

---
+++ api/posts.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const authMiddleware = require("../middleware/authMiddleware");
+const UserModel = require("../models/UserModel");
+const PostModel = require("../models/PostModel");
+const FollowerModel = require("../models/FollowerModel");
+
+
+// CREATE A POST
+
+router.post("/", authMiddleware, async (req, res) => {
+  const { text, location, picUrl } = req.body;
+
+  if (text.length < 1)
+    return res.status(401).send("Text must be atleast 1 character");
+
+  try {
+    const newPost = {
+      user: req.userId,
+      text
+    };
+    if (location) newPost.location = location;
+    if (picUrl) newPost.picUrl = picUrl;
+
+    const post = await new PostModel(newPost).save();
+
+    return res.json(post);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send(`Server error`);
+  }
+});
+
+module.exports = router;

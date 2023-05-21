e3d5739f90758a6da49b38626da9b588ea182823
panzhilov <panzhilov@gmail.com>
2023-05-21 22:14:19 +0300

    add get all, delete one

--- api/posts.js
+++ api/posts.js
@@ -5,7 +5,6 @@ const UserModel = require("../models/UserModel");
 const PostModel = require("../models/PostModel");
 const FollowerModel = require("../models/FollowerModel");
 
-
 // CREATE A POST
 
 router.post("/", authMiddleware, async (req, res) => {
@@ -30,5 +29,72 @@ router.post("/", authMiddleware, async (req, res) => {
     return res.status(500).send(`Server error`);
   }
 });
+
+//GET ALL POSTS
+
+router.get("/", authMiddleware, async (req, res) => {
+  try {
+    const posts = await PostModel.find()
+      .sort({ createdAt: -1 })
+      .populate("user")
+      .populate("comments.user");
+    return res.json(posts);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server error");
+  }
+});
+
+//GET POST BY ID
+
+router.get("/:postId", authMiddleware, async (req, res) => {
+  try {
+    const post = await PostModel.findById(req.params.postId);
+
+    if (!post) {
+      return res.status(404).send("Post not found");
+    }
+
+    return res.json(post);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Server error");
+  }
+});
+
+// DELETE POST
+
+router.delete("/:postId", authMiddleware, async (req, res) => {
+  try {
+    const { userId } = req;
+
+    const { postId } = req.params;
+
+    const post = await PostModel.findById(postId);
+
+    if (!post) {
+      return res.status(404).send("Post not found");
+    }
+
+    const user = await UserModel.findById(userId);
+
+    if (post.user.toString() !== userId) {
+      if (user.role === "root") {
+        await UserModel.deleteOne(post);
+        return res.status(200).send("Post deleted succsessfully");
+      } else {
+        return res.status(401).send("Unauthorized");
+      }
+    }
+
+    await UserModel.deleteOne(post);
+    return res.status(200).send("Post deleted succsessfully");
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send("Server error");
+  }
+});
+
+// LIKE A POST
 
 module.exports = router;

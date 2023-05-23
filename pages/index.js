803c30751eeaa8e71c0bd3a1cdf6d14dde54bd37
panzhilov <panzhilov@gmail.com>
2023-05-23 23:40:46 +0300

    add posts to index

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,57 @@
 import axios from "axios";
 import baseUrl from "../utils/baseUrl";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import CreatePost from "../components/Post/CreatePost";
+import CardPost from "../components/Post/CardPost";
+import { Segment } from "semantic-ui-react";
+import { parseCookies } from "nookies";
+import { NoPosts } from "../components/Layout/NoData";
 
+function Index({ user, postsData, errorLoading }) {
+  const [posts, setPosts] = useState(postsData);
+  const [showToastr, setShowToastr] = useState(false);
 
-function Index({user, userFollowStats}) {
+  useEffect(() => {
+    document.title = `Welcome ,${user.name.split(" ")[0]}`;
+  }, []);
 
-    useEffect(() => {
-        document.title = `Welcome ,${user.name.split(" ")[0]}`;
-    },[])
+  if (posts === 0 || errorLoading) {
+    return <NoPosts />;
+  }
 
-    return( 
-        <div>
-         Homepage
-        </div>
-    )
+  return (
+    <>
+      <Segment>
+        <CreatePost user={user} setPosts={setPosts} />
+
+        {posts.map((post) => (
+          <CardPost
+            key={post._id}
+            post={post}
+            user={user}
+            setPosts={setPosts}
+            setShowToastr={setShowToastr}
+          />
+        ))}
+      </Segment>
+    </>
+  );
 }
 
-
+Index.getInitialProps = async ctx => {
+    try {
+      const { token } = parseCookies(ctx);
+  
+      const res = await axios.get(`${baseUrl}/api/posts`, {
+        headers: { Authorization: token },
+        params: { pageNumber: 1 }
+      });
+  
+      return { postsData: res.data };
+    } catch (error) {
+      return { errorLoading: true };
+    }
+  };
+  
 
 export default Index;

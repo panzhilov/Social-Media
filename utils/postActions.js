a04a1d8914b7446d682a6e08e6b508ba2f4e22ec
panzhilov <panzhilov@gmail.com>
2023-05-24 10:34:01 +0300

    import like and unlike

--- utils/postActions.js
+++ utils/postActions.js
@@ -30,9 +30,26 @@ export const submitNewPost = async (
 export const deletePost = async (postId, setPosts, setShowToastr) => {
   try {
     await Axios.delete(`/${postId}`);
-    setPosts(prev => prev.filter(post => post._id !== postId));
+    setPosts((prev) => prev.filter((post) => post._id !== postId));
     setShowToastr(true);
   } catch (error) {
     alert(catchErrors(error));
   }
 };
+
+export const likePost = async (postId, userId, setLikes, like = true) => {
+  try {
+    if (like) {
+      await Axios.post(`/like/${postId}`);
+      setLikes(prev => [...prev, { user: userId }]);
+    }
+  
+    else if (!like) {
+      await Axios.put(`/unlike/${postId}`);
+      setLikes(prev => prev.filter(like => like.user !== userId));
+    }
+  } catch (error) {
+    alert(catchErrors(error));
+  }
+};
+

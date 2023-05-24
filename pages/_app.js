d9ca2bbe66bcf70a1425e60f6fea2943ae43cc41
panzhilov <panzhilov@gmail.com>
2023-05-24 10:12:32 +0300

    import toastify

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import baseUrl from "../utils/baseUrl";
 import { redirectUser } from "../utils/authUser";
 import Layout from "../components/Layout/Layout";
 import "react-toastify/dist/ReactToastify.css";
+import "react-toastify/dist/ReactToastify.css"
 import "semantic-ui-css/semantic.min.css";
 
 function MyApp({ Component, pageProps }) {

4b019c2ab091f6d433a69b2d0a3f34c30fdec6ec
panzhilov <panzhilov@gmail.com>
2023-06-06 16:40:48 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     env: {
-        CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/delo4uxku/image/upload"
+        CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/delo4uxku"
     }
 }

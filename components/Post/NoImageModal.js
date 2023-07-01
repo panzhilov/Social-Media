5fed67c6a102accfb926170bf602f9ea05e96dd9
panzhilov <panzhilov@gmail.com>
2023-07-01 11:09:12 +0300

    Create component for noimage

---
+++ components/Post/NoImageModal.js
@@ -0,0 +1,94 @@
+import { Modal, Grid, Image, Card, Icon, Divider } from "semantic-ui-react";
+import PostComment from "./PostComment";
+import CommentInputField from "./CommentInputField";
+import calculateTime from "../../utils/calculateTime";
+import Link from "next/link";
+import { likePost } from "../../utils/postActions";
+import LikesList from "./LikesList";
+
+function NoImageModal({
+  post,
+  user,
+  setLikes,
+  likes,
+  isLiked,
+  comments,
+  setComments
+}) {
+  return (
+    <Card fluid>
+      <Card.Content>
+        <Image floated="left" avatar src={post.user.profilePicUrl} />
+
+        <Card.Header>
+          <Link href={`/${post.user.username}`}>{post.user.name}</Link>
+        </Card.Header>
+
+        <Card.Meta>{calculateTime()}</Card.Meta>
+
+        {post.location && <Card.Meta content={post.location} />}
+
+        <Card.Description
+          style={{
+            fontSize: "17px",
+            letterSpacing: "0.1px",
+            wordSpacing: "0.35px"
+          }}
+        >
+          {post.text}
+        </Card.Description>
+      </Card.Content>
+
+      <Card.Content extra>
+        <Icon
+          name={isLiked ? "heart" : "heart outline"}
+          color="red"
+          style={{ cursor: "pointer" }}
+          onClick={() =>
+            likePost(post._id, user._id, setLikes, isLiked ? false : true)
+          }
+        />
+
+        <LikesList
+          postId={post._id}
+          trigger={
+            likes.length > 0 && (
+              <span className="spanLikesList">
+                {`${likes.length} ${likes.length === 1 ? "like" : "likes"}`}
+              </span>
+            )
+          }
+        />
+
+        <Divider hidden />
+
+        <div
+          style={{
+            overflow: "auto",
+            height: "60px",
+            marginBottom: "8px"
+          }}
+        >
+          {comments.length > 0 &&
+            comments.map((comment, i) => (
+              <PostComment
+                key={comment._id}
+                comment={comment}
+                postId={post._id}
+                user={user}
+                setComments={setComments}
+              />
+            ))}
+        </div>
+
+        <CommentInputField
+          postId={post._id}
+          user={user}
+          setComments={setComments}
+        />
+      </Card.Content>
+    </Card>
+  );
+}
+
+export default NoImageModal;
